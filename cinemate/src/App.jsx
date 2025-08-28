f53760adfafbb36bda60d524b435ac06d87af3cf
KEVIN NSHUTI <nkevin881@gmail.com>
2025-08-28 15:26:01 +0200

    fixing details page and adding a contact form

--- cinemate/src/App.jsx
+++ cinemate/src/App.jsx
@@ -3,12 +3,12 @@ import DetailPage from "./components/DetailsPage";
 import SearchBar from "./components/SearchBar";
 import ContactPage from "./components/ContactPage";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/front-end-capstone-project/" element={<HomePage />} />
           <Route path="/movie/:title" element={<DetailPage />} />
@@ -18,7 +18,7 @@ function App() {
           />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
